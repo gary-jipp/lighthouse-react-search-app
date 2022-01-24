5ccbac69a509f83e9df74902e64915fde8b3d745
Gary J <github@accu.ca>
2022-01-23 22:47:24 -0800

    clean

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import { useState } from 'react';
-import Input from 'components/Input';
-import SearchList from 'components/SearchList';
 import 'App.css';
-const BASEURL = 'https://hn.algolia.com/api/v1/search?query=';
 
 export default function App() {
-  const [url, setUrl] = useState();
-
-  const onSubmit = function(input) {
-    setUrl(BASEURL + input);
-  };
 
   return (
     <div className="App">
-      <h1>Search Page</h1>
-      <Input label='Search' onSubmit={onSubmit} />
-      <SearchList url={url} />
+      <h2>Hello React</h2>
     </div>
   );
 }
